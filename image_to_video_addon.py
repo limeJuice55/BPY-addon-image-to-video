648615dc3d24633787ca4dfe7897e3d939ef5740
limeJuice55 <83681528+limeJuice55@users.noreply.github.com>
2022-03-23 11:26:42 +1100

    Am converting my bad uploads to proper committing
    I am fixing the issues of uploading a new file every time by committing and pushing instead.
    
    Changes:
    
    Added the UI class
    defined the location of the UI class
    added the register/unregister process
    added text, an icon and a button

--- image_to_video_addon.py
+++ image_to_video_addon.py
@@ -1,1 +1,33 @@
 import bpy
+
+
+class ImageSeqConverter(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Image Sequence Converter"
+    bl_idname = "IMG_PT_CONVERT"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "render"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+
+        row = layout.row()
+        row.label(text="Convert Image Sequence to Video", icon='IMAGE_DATA')
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add")
+
+
+def register():
+    bpy.utils.register_class(ImageSeqConverter)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImageSeqConverter)
+
+
+if __name__ == "__main__":
+    register()
